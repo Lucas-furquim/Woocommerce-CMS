52940af5bb7ce694ebb8861d5afcda0a2ac61e44
Furquim~ <lucasviniciusfurquim@gmail.com>
2025-04-28 18:15:53 -0300

    minha conta

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <?php if(have_posts()) { while (have_posts()) { the_post(); ?>
 
   <h1 class='titulo'><?php the_title(); ?></h1>
-  <main class='container container-index woocommerce'><?php the_content(); ?></main>
+  <main class='container container-index'><?php the_content(); ?></main>
 
 <?php } } ?>
 
